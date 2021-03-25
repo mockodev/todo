495f3a4b519b21b0eba8281d532ef22da13578d9
mocko <daniel.schmidtke@web.de>
2021-03-25 17:11:50 +0100

    sdfsdfg

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import "./App.css";
 
+
 function App() {
   return (
-     <div className="app-container">
-       <div className="app">
-       <h1>My Todo list</h1>
-     </div>
-     </div>
+    <div className="app-container">
+      <div className="app">
+        <h1>My Todo bla</h1>
+      </div>
+    </div>
   );
 }
 
